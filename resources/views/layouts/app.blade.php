5c30ca83fab64b6bb989b131a0e060e318616761
amani-sirelkhatim <amanisirelkhatim1509@gmail.com>
2025-12-27 17:24:28 +0200

    loginlogo fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="/css/login.css">
     <style>
 
     </style>
